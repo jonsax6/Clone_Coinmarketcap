c42985a288731344042e01ddd1c1c837b7090f0a
jonsax6 <jdc.saxjunky@gmail.com>
2020-08-25 19:36:11 -0400

    ongoing work on populateExchangeTab()
    ongoing work on populate exchange tab function.

--- logic.js
+++ logic.js
@@ -24,23 +24,32 @@ $( document ).ready(function() {
         let coin = CMC_DATA[i];
         coinID_Obj_Name[coin.name] = coin.id;
     }
-    //let ethChart = `${SPARKLINE_BASE}${coinID_Obj["ETH"]}.png`
-    
+
+    async function current_BTC_price(){
+        const CRYPTO_MARKETS = `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+        
+        let res = await fetch(CRYPTO_MARKETS);    
+        const data = await res.json();       
+        let BTC_USD_price = data[0].current_price;
+        return BTC_USD_price;   
+      }
+     
     
 
+
     function populateCoinsTable(){
-        $("#crypto-page-title").html("");
-        $("#crypto-page-title").append(
+        $("#nav-exchanges").hide();
+        $("#crypto-page-title").html(
             `                    
-            <div class="row justify-content-center full-height">
+            <div class="row justify-content-center mt-3 full-height">
                 <h4 class="align-self-center"><strong>Top 100 Cryptocurrencies by Market Capitalization</strong></h4>
             </div>
             `
             )
-        let COINS_MARKETS = `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${coinPG}&sparkline=false`;
+        let COINS_MARKETS = `${BASE_URL}/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=${coinPG}&sparkline=true`;
         fetch(COINS_MARKETS)
-          .then(res => {
-          res.json().then( data => {
+            .then(res => {
+            res.json().then( data => {
             for(i = 0; i < data.length; i++){
                 let coinData = data[i];
                 const MarketCap = coinData.market_cap ? Number(coinData.market_cap.toFixed(2)) : "-";
@@ -48,6 +57,7 @@ $( document ).ready(function() {
                 const volume = coinData.total_volume ? Number(coinData.total_volume.toFixed(2)) : "-";
                 const cirSuppy = coinData.circulating_supply ? Number(coinData.circulating_supply.toFixed(2)) : "-";
                 const coinDelta = coinData.price_change_24h ? Number(coinData.price_change_24h.toFixed(2)) : "-";
+                const sparkData = coinData.sparkline_in_7d.price;
                 const coinSymbol = coinData.symbol;
                 const coinName = coinData.name;
                 const coin_ID = coinData.id;
@@ -64,118 +74,92 @@ $( document ).ready(function() {
                     ID_url = "1";
                 }
                 //table dynamically created, data feed from fetch(COINS_MARKETS)
-                $("#marketsTable").append(
+                $("#markets-table").append(
                     `<tr>
                         <th scope="row">${coinData.market_cap_rank}</td>
                         <td><b><img src="${coinData.image}" style="height: 1em;">&nbsp;&nbsp;${coinData.name}</b></td>
-                        <td>${formatter.format(MarketCap)}</td>
-                        <td>${formatter.format(coinPrice)}</td>
-                        <td>${formatter.format(volume)}</td>
-                        <td>${formatter.format(cirSuppy)}</td>
-                        <td>${formatter.format(coinDelta)}</td>
+                        <td class="text-right">${formatter.format(MarketCap)}</td>
+                        <td class="text-right">${formatter.format(coinPrice)}</td>
+                        <td class="text-right">${formatter.format(volume)}</td>
+                        <td class="text-right">${formatter.format(cirSuppy)}&nbsp;${capSymbol}</td>
+                        <td class="text-right">${formatter.format(coinDelta)}</td>
                         <td><img src=${SPARKLINE_BASE}${ID_url}.png style="height: 2em;"></td>
-                        <td>
-                        <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                        </svg></a>
-                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="https://coinmarketcap.com/headlines/">Add to Watchlist</a>
-                                <a class="dropdown-item" href="https://coinmarketcap.com/currencies/bitcoin/">view Chart</a>
-                                <a class="dropdown-item" href="https://coinmarketcap.com/currencies/bitcoin/markets/">View Markets</a>
-                                <a class="dropdown-item" href="https://coinmarketcap.com/currencies/bitcoin/historical-data/">View Historical Data</a>
-                            </div>
-                            <a href
-                        </td>
                     </tr>`
                 )
                 
 
                 
             }
-           $("#prev-page").html("");
-          })
-        })
+            })
+            })
+            .catch(function(err){
+                alert("fetch() data error");
+            })
     }
     function populateExchangeTable(){
-        $("#crypto-page-title").html("");
-        $("#crypto-page-title").append(
+        $("#nav-cryptocurrency").hide();
+        $("#crypto-page-title").html(
             `                    
-            <div class="row justify-content-center full-height">
-                <h4 class="align-self-center"><strong>Top Cryptocurrency Spot Exchanges</strong></h4>
+            <div class="row border justify-content-center mt-3">
+                <h4 class="align-bottom"><strong>Top Cryptocurrency Spot Exchanges</strong></h4>
             </div>
-            <div class="row justify-content-center full-height">
-                <h6 class="text-secondary font-weight-light">CoinMarketCap ranks the top cryptocurrency exchanges based on traffic, liquidity, trading volumes of spot markets.</h6>
+            <div class="row border justify-content-center full-height">
+                <h6 class="align-top text-secondary font-weight-light">CoinMarketCap ranks the top cryptocurrency exchanges based on traffic, liquidity, trading volumes of spot markets.</h6>
             </div>
             `
-            )
-            let EXCHANGES = `${BASE_URL}/exchanges?per_page=100&page=${exchangePG}`;
+           )
+        let EXCHANGES = `${BASE_URL}/exchanges?per_page=100&page=${exchangePG}`;
         fetch(EXCHANGES)
-          .then(res => {
-          res.json().then( data => {
+            .then(res => {
+            res.json().then( async data => {
             for(i = 0; i < 100; i++){
                 let exchangeData = data[i];
-                const MarketCap = exchangeData.market_cap ? Number(exchangeData.market_cap.toFixed(2)) : "-";
-                const coinPrice = exchangeData.current_price ? Number(exchangeData.current_price.toFixed(2)) : "-";
-                const volume = exchangeData.trade_volume_24h_btc ? Number(exchangeData.trade_volume_24h_btc.toFixed(2)) : "-";
-                const cirSuppy = exchangeData.circulating_supply ? Number(exchangeData.circulating_supply.toFixed(2)) : "-";
-                const coinDelta = exchangeData.price_change_24h ? Number(exchangeData.price_change_24h.toFixed(2)) : "-";
+                const exName = exchangeData.name;
+                const exCountry = exchangeData.country;
+                const exYear = exchangeData.year_established;
+                const volumeBTC = exchangeData.trade_volume_24h_btc ? Number(exchangeData.trade_volume_24h_btc).toFixed(2) : "-";
+                var BTC_price = await current_BTC_price();
+                const volumeUSD = Number((volumeBTC * BTC_price)).toFixed(2);
+                const exTrust = exchangeData.trust_score;
 
-              // CREATE A ROW AND APPEND TO TABLE
-                // $("#marketsTable").append(
-                //     `<tr>
-                //         <th scope="row">${i+rank}</td>
-                //         <td><b><img src="${exchangeData.image}" style="height: 1em;">&nbsp;&nbsp;${coinData.name}</b></td>
-                //         <td>${formatter.format(MarketCap)}</td>
-                //         <td>${formatter.format(coinPrice)}</td>
-                //         <td>${formatter.format(volume)}</td>
-                //         <td>${formatter.format(cirSuppy)}</td>
-                //         <td>${formatter.format(coinDelta)}</td>
-                //         <td><img src=${sparkineUrl} style="height: 2em;"></td>
-                //         <td>
-                //         <a class="dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                //         <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-three-dots" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                //         <path fill-rule="evenodd" d="M3 9.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3zm5 0a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3z"/>
-                //         </svg></a>
-                //             <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                //                 <a class="dropdown-item" href="https://coinmarketcap.com/headlines/">Add to Watchlist</a>
-                //                 <a class="dropdown-item" href="https://coinmarketcap.com/currencies/bitcoin/">view Chart</a>
-                //                 <a class="dropdown-item" href="https://coinmarketcap.com/currencies/bitcoin/markets/">View Markets</a>
-                //                 <a class="dropdown-item" href="https://coinmarketcap.com/currencies/bitcoin/historical-data/">View Historical Data</a>
-                //             </div>
-                //             <a href
-                //         </td>
-                //     </tr>`
-                //)
-                
-
-                
+                $("#markets-table").append(
+                    `<tr>
+                        <th scope="row">${exchangeData.trust_score_rank}</td>
+                        <td><b><img src="${exchangeData.image}" style="height: 1em;">&nbsp;&nbsp;${exchangeData.name}</b></td>
+                        <td class="text-right">${exCountry}</td>
+                        <td>${exYear}}</td>
+                        <td>${formatter.format(volumeUSD)}</td>
+                        <td>${exTrust}</td>
+                    </tr>`
+                )
             }
           })
         })
     }
 
+
+    
 $("#previous-page").hide();
 $("#previous-page-btm").hide();
 populateCoinsTable();
 
 
 $("#next-page").click(function(){
-    $("#marketsTable").html("");
+    $("#markets-table").html("");
     $("#previous-page").show();
     $("#previous-page-btm").show();
     coinPG++;
     populateCoinsTable();
 })
 $("#next-page-btm").click(function(){
-    $("#marketsTable").html("");
+    $("#markets-table").html("");
     $("#previous-page").show();
     $("#previous-page-btm").show();
     coinPG++;
     populateCoinsTable();
 })
 $("#previous-page").click(function(){
-    $("#marketsTable").html("");
+    $("#markets-table").html("");
     if(coinPG < 3){
         $("#previous-page").hide();
         $("#previous-page-btm").hide();
@@ -184,7 +168,7 @@ $("#previous-page").click(function(){
     populateCoinsTable();
 })
 $("#previous-page-btm").click(function(){
-    $("#marketsTable").html("");
+    $("#markets-table").html("");
     if(coinPG < 3){
         $("#previous-page").hide();
         $("#previous-page-btm").hide();
